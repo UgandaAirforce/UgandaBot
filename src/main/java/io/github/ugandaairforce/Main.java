fb415f36b04cf6a41d6a9c997a5e1aeb16f3730c
StealWonders <StealWonders@users.noreply.github.com>
2018-08-23 21:33:43 +0200

    base classes added

---
+++ src/main/java/io/github/ugandaairforce/Main.java
@@ -0,0 +1,10 @@
+package io.github.ugandaairforce;
+
+public class Main {
+
+	public static UgandaBot INSTANCE;
+
+	public static void main(String[] args) {
+		INSTANCE = new UgandaBot();
+	}
+}
--- src/main/java/io/github/ugandaairforce/UgandaBot.java
+++ src/main/java/io/github/ugandaairforce/UgandaBot.java
@@ -1,8 +1,38 @@
 package io.github.ugandaairforce;
 
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+
+import javax.security.auth.login.LoginException;
+
 public class UgandaBot {
 
-    public static void main(String[] args) {
-        System.out.println(UgandaBotProperties.getProperty("bot.game"));
-    }
+	JDA jda;
+	String token;
+
+	public UgandaBot() {
+
+		try {
+			init();
+		} catch (LoginException | RateLimitedException | InterruptedException exception) {
+			exception.printStackTrace();
+		}
+	}
+
+    private void init() throws LoginException, RateLimitedException, InterruptedException {
+
+		System.out.println("Initializing bot...");
+    	UgandaBotProperties.getProperty("bot.token");
+
+    	if (token != null) {
+			try {
+				jda = new JDABuilder(AccountType.BOT).setToken("token").build();
+			} catch (LoginException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
 }
