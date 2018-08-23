2bb6998bbccc0916252d38f8180fc4d1befccb17
FatusCatus <suchdirt@gmail.com>
2018-08-23 20:41:48 +0200

    Added Property System

--- src/main/java/io/github/ugandaairforce/UgandaBot.java
+++ src/main/java/io/github/ugandaairforce/UgandaBot.java
@@ -3,5 +3,6 @@ package io.github.ugandaairforce;
 public class UgandaBot {
 
     public static void main(String[] args) {
+        System.out.println(UgandaBotProperties.getProperty("bot.game"));
     }
 }
